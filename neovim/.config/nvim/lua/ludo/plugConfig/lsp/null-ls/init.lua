a25112389a689f8dc7355289315cc5097fac3579
LudoPinelli <93927273+LudoPinelli@users.noreply.github.com>
2021-12-26 04:30:54 +0100

    .sh, .md and .toml formatting and .sh diagnostics added to null-ls

--- neovim/.config/nvim/lua/ludo/plugConfig/lsp/null-ls/init.lua
+++ neovim/.config/nvim/lua/ludo/plugConfig/lsp/null-ls/init.lua
@@ -1,10 +1,15 @@
 local null_ls = require("null-ls")
 
-  local formatting = null_ls.builtins.formatting
+local formatting = null_ls.builtins.formatting
+local diagnostics = null_ls.builtins.diagnostics
 
 local sources = {
 	formatting.stylua,
 	formatting.autopep8,
+	formatting.prettier,
+	formatting.shellharden,
+	formatting.taplo,
+	diagnostics.shellcheck,
 }
 
 null_ls.setup({
