d109e902f4d83942eca3c321e5d399cce05ec376
LudoPinelli <93927273+LudoPinelli@users.noreply.github.com>
2022-01-07 21:36:21 +0100

    simplify: os selection for which-key mappings

--- neovim/.config/nvim/lua/plugConfig/which-key/keymaps.lua
+++ neovim/.config/nvim/lua/plugConfig/which-key/keymaps.lua
@@ -2,57 +2,85 @@ require("plugConfig.toggleterm.custom_terms")
 require("plugConfig.telescope.custom_pickers")
 
 local wk = require("which-key")
+
+wk.register({
+	["<Leader>"] = {
+		["a"] = { "<Cmd>Alpha<CR>", "䀘 Landing page" },
+		["c"] = { "<Cmd>BufferClose<CR>", "  Close Buffer" },
+		["e"] = { "<Cmd>lua require('lir.float').toggle()<CR>", "פּ  Explorer" },
+		["h"] = { "<Cmd>nohlsearch<CR>", "  No Highlight" },
+		["?"] = { ":<C-U>Cheatsheet<CR>", "  Cheat Sheets" },
+		["z"] = { "<Cmd>set spell!<CR>", "暈 Toggle Spellcheck" },
+		["W"] = { "<Cmd>cd %:p:h<CR>:pwd<CR>", "  Set Working Directory" },
+		["C"] = { "<Cmd>lua nvim_config()<CR>", "  Config Files" },
+		["n"] = { "<Cmd>Telescope neoclip<CR>", "  Clipboard" },
+		["i"] = { "`.", "  Back to last insert" },
+		["p"] = { "<Cmd>BufferPick<CR>", "  Pick a buffer" },
+
+		g = {
+			name = "   Git",
+
+			b = { "<Cmd>Telescope git_branches<CR>", "  Branches" },
+			c = { "<Cmd>Telescope git_commits<CR>", "  Commits" },
+			g = { "<Cmd>lua lazygit_toggle()<CR>", "  LazyGit" },
+			f = { "<Cmd>Telescope git_files<CR>", "Git files" },
+			j = { "<Cmd>lua require('gitsigns').next_hunk()<CR>", "Next Hunk" },
+			k = { "<Cmd>lua require('gitsigns').prev_hunk()<CR>", "Prev Hunk" },
+			l = { "<Cmd>lua require('gitsigns').blame_line{full=true}<CR>", "Blame line" },
+			o = { "<Cmd>Telescope git_status<CR>", "  Open changed file" },
+			p = { "<Cmd>lua require('gitsigns').preview_hunk()<CR>", "Preview Hunk" },
+			r = { "<Cmd>lua require('gitsigns').reset_hunk()<CR>", "Reset Hunk" },
+			R = { "<Cmd>lua require('gitsigns').reset_buffer()<CR>", "Reset Buffer" },
+			s = { "<Cmd>lua require('gitsigns').stage_hunk()<CR>", "Stage Hunk" },
+			u = { "<Cmd>lua require('gitsigns').undo_stage_hunk()<CR>", "Undo Stage Hunk" },
+		},
+
+		l = {
+			name = "   LSP",
+
+			a = { "<Cmd>lua vim.lsp.buf.code_action()<CR>", "Code Action" },
+			d = { "<Cmd>lua vim.diagnostic.open_float()<CR>", "Diagnostics in float window" },
+			D = { "<Cmd>Telescop diagnostics<CR>", "Diagnostics in Telescope" },
+			f = { "<Cmd>lua vim.lsp.buf.formatting()<CR>", "  Format" },
+			i = { "<Cmd>LspInfo<CR>", "  Info" },
+			l = { "<Cmd>lua vim.lsp.codelens.run()<CR>", "CodeLens Action" },
+			L = { "<Cmd>lua vim.diagnostic.setloclist()<CR>", "Location list" },
+			q = { "<Cmd>lua vim.diagnostic.setqflist()<CR>", "Send to Quickfix List" },
+			r = { "<Cmd>lua vim.lsp.buf.rename()<CR>", "凜  Rename" },
+			R = { "<Cmd>lua vim.lsp.buf.references()<CR>", "Ref. of word under cursor" },
+			s = { "<Cmd>lua vim.lsp.buf.document_symbol()<CR>", "  Document Symbols" },
+			S = { "<Cmd>Telescope lsp_dynamic_workspace_symbols<CR>", "  Workspace Symbols" },
+			t = { "<Cmd>lua vim.lsp.buf.type_definition()<CR>", "Type Definition" },
+		},
+
+		t = {
+			name = "   Terminal",
+
+			f = { "<Cmd>ToggleTerm direction=float<CR>", "  Float" },
+			g = { "<Cmd>lua lazygit_toggle()<CR>", "  LazyGit" },
+			h = { "<Cmd>ToggleTerm size=10 direction=horizontal<CR>", "  Horizontal" },
+			n = { "<Cmd>lua node_toggle()<CR>", "  Node.js" },
+			p = { "<Cmd>lua python_toggle()<CR>", "   Python" },
+			t = { "<Cmd>lua htop_toggle()<CR>", "   Htop" },
+			v = { "<Cmd>ToggleTerm size=80 direction=vertical<CR>", "   Vertical" },
+			w = { "<Cmd>lua cargo_watch_toggle()<CR>", "   Cargo Watch" },
+		},
+
+		x = {
+			name = "   Lists",
+
+			d = { "<Cmd>Trouble document_diagnostics<CR>", "Document Diagnostics" },
+			l = { "<Cmd>Trouble loclist<CR>", "Loclist" },
+			q = { "<Cmd>Trouble quickfix<CR>", "Quickfix List" },
+			r = { "<Cmd>Trouble lsp_references<CR>", "Ref. of word under cursor" },
+			w = { "<Cmd>Trouble workspace_diagnostics<CR>", "Workspace Diagnostics" },
+		},
+	},
+})
+
 if jit.os ~= "Windows" then
 	wk.register({
 		["<Leader>"] = {
-			["a"] = { "<Cmd>Alpha<CR>", "䀘 Landing page" },
-			["c"] = { "<Cmd>BufferClose<CR>", "  Close Buffer" },
-			["e"] = { "<Cmd>lua require('lir.float').toggle()<CR>", "פּ  Explorer" },
-			["h"] = { "<Cmd>nohlsearch<CR>", "  No Highlight" },
-			["?"] = { ":<C-U>Cheatsheet<CR>", "  Cheat Sheets" },
-			["z"] = { "<Cmd>set spell!<CR>", "暈 Toggle Spellcheck" },
-			["W"] = { "<Cmd>cd %:p:h<CR>:pwd<CR>", "  Set Working Directory" },
-			["C"] = { "<Cmd>lua nvim_config()<CR>", "  Config Files" },
-			["n"] = { "<Cmd>Telescope neoclip<CR>", "  Clipboard" },
-			["i"] = { "`.", "  Back to last insert" },
-			["p"] = { "<Cmd>BufferPick<CR>", "  Pick a buffer" },
-
-			g = {
-				name = "   Git",
-
-				b = { "<Cmd>Telescope git_branches<CR>", "  Branches" },
-				c = { "<Cmd>Telescope git_commits<CR>", "  Commits" },
-				g = { "<Cmd>lua lazygit_toggle()<CR>", "  LazyGit" },
-				f = { "<Cmd>Telescope git_files<CR>", "Git files" },
-				j = { "<Cmd>lua require('gitsigns').next_hunk()<CR>", "Next Hunk" },
-				k = { "<Cmd>lua require('gitsigns').prev_hunk()<CR>", "Prev Hunk" },
-				l = { "<Cmd>lua require('gitsigns').blame_line{full=true}<CR>", "Blame line" },
-				o = { "<Cmd>Telescope git_status<CR>", "  Open changed file" },
-				p = { "<Cmd>lua require('gitsigns').preview_hunk()<CR>", "Preview Hunk" },
-				r = { "<Cmd>lua require('gitsigns').reset_hunk()<CR>", "Reset Hunk" },
-				R = { "<Cmd>lua require('gitsigns').reset_buffer()<CR>", "Reset Buffer" },
-				s = { "<Cmd>lua require('gitsigns').stage_hunk()<CR>", "Stage Hunk" },
-				u = { "<Cmd>lua require('gitsigns').undo_stage_hunk()<CR>", "Undo Stage Hunk" },
-			},
-
-			l = {
-				name = "   LSP",
-
-				a = { "<Cmd>lua vim.lsp.buf.code_action()<CR>", "Code Action" },
-				d = { "<Cmd>lua vim.diagnostic.open_float()<CR>", "Diagnostics in float window" },
-				D = { "<Cmd>Telescop diagnostics<CR>", "Diagnostics in Telescope" },
-				f = { "<Cmd>lua vim.lsp.buf.formatting()<CR>", "  Format" },
-				i = { "<Cmd>LspInfo<CR>", "  Info" },
-				l = { "<Cmd>lua vim.lsp.codelens.run()<CR>", "CodeLens Action" },
-				L = { "<Cmd>lua vim.diagnostic.setloclist()<CR>", "Location list" },
-				q = { "<Cmd>lua vim.diagnostic.setqflist()<CR>", "Send to Quickfix List" },
-				r = { "<Cmd>lua vim.lsp.buf.rename()<CR>", "凜  Rename" },
-				R = { "<Cmd>lua vim.lsp.buf.references()<CR>", "Ref. of word under cursor" },
-				s = { "<Cmd>lua vim.lsp.buf.document_symbol()<CR>", "  Document Symbols" },
-				S = { "<Cmd>Telescope lsp_dynamic_workspace_symbols<CR>", "  Workspace Symbols" },
-				t = { "<Cmd>lua vim.lsp.buf.type_definition()<CR>", "Type Definition" },
-			},
-
 			s = {
 				name = "   Search",
 
@@ -75,82 +103,11 @@ if jit.os ~= "Windows" then
 				R = { "<Cmd>Telescope registers<CR>", "  Registers" },
 				t = { "<Cmd>Telescope live_grep<CR>", "  Text" },
 			},
-
-			t = {
-				name = "   Terminal",
-
-				f = { "<Cmd>ToggleTerm direction=float<CR>", "  Float" },
-				g = { "<Cmd>lua lazygit_toggle()<CR>", "  LazyGit" },
-				h = { "<Cmd>ToggleTerm size=10 direction=horizontal<CR>", "  Horizontal" },
-				n = { "<Cmd>lua node_toggle()<CR>", "  Node.js" },
-				p = { "<Cmd>lua python_toggle()<CR>", "   Python" },
-				t = { "<Cmd>lua htop_toggle()<CR>", "   Htop" },
-				v = { "<Cmd>ToggleTerm size=80 direction=vertical<CR>", "   Vertical" },
-				w = { "<Cmd>lua cargo_watch_toggle()<CR>", "   Cargo Watch" },
-			},
-
-			x = {
-				name = "   Lists",
-
-				d = { "<Cmd>Trouble document_diagnostics<CR>", "Document Diagnostics" },
-				l = { "<Cmd>Trouble loclist<CR>", "Loclist" },
-				q = { "<Cmd>Trouble quickfix<CR>", "Quickfix List" },
-				r = { "<Cmd>Trouble lsp_references<CR>", "Ref. of word under cursor" },
-				w = { "<Cmd>Trouble workspace_diagnostics<CR>", "Workspace Diagnostics" },
-			},
 		},
 	})
 else
 	wk.register({
 		["<Leader>"] = {
-			["a"] = { "<Cmd>Alpha<CR>", "䀘 Landing page" },
-			["c"] = { "<Cmd>BufferClose<CR>", "  Close Buffer" },
-			["e"] = { "<Cmd>lua require('lir.float').toggle()<CR>", "פּ  Explorer" },
-			["h"] = { "<Cmd>nohlsearch<CR>", "  No Highlight" },
-			["?"] = { ":<C-U>Cheatsheet<CR>", "  Cheat Sheets" },
-			["z"] = { "<Cmd>set spell!<CR>", "暈 Toggle Spellcheck" },
-			["W"] = { "<Cmd>cd %:p:h<CR>:pwd<CR>", "  Set Working Directory" },
-			["C"] = { "<Cmd>lua nvim_config()<CR>", "  Config Files" },
-			["n"] = { "<Cmd>Telescope neoclip<CR>", "  Clipboard" },
-			["i"] = { "`.", "  Back to last insert" },
-			["p"] = { "<Cmd>BufferPick<CR>", "  Pick a buffer" },
-
-			g = {
-				name = "   Git",
-
-				b = { "<Cmd>Telescope git_branches<CR>", "  Branches" },
-				c = { "<Cmd>Telescope git_commits<CR>", "  Commits" },
-				g = { "<Cmd>lua lazygit_toggle()<CR>", "  LazyGit" },
-				f = { "<Cmd>Telescope git_files<CR>", "Git files" },
-				j = { "<Cmd>lua require('gitsigns').next_hunk()<CR>", "Next Hunk" },
-				k = { "<Cmd>lua require('gitsigns').prev_hunk()<CR>", "Prev Hunk" },
-				l = { "<Cmd>lua require('gitsigns').blame_line{full=true}<CR>", "Blame line" },
-				o = { "<Cmd>Telescope git_status<CR>", "  Open changed file" },
-				p = { "<Cmd>lua require('gitsigns').preview_hunk()<CR>", "Preview Hunk" },
-				r = { "<Cmd>lua require('gitsigns').reset_hunk()<CR>", "Reset Hunk" },
-				R = { "<Cmd>lua require('gitsigns').reset_buffer()<CR>", "Reset Buffer" },
-				s = { "<Cmd>lua require('gitsigns').stage_hunk()<CR>", "Stage Hunk" },
-				u = { "<Cmd>lua require('gitsigns').undo_stage_hunk()<CR>", "Undo Stage Hunk" },
-			},
-
-			l = {
-				name = "   LSP",
-
-				a = { "<Cmd>lua vim.lsp.buf.code_action()<CR>", "Code Action" },
-				d = { "<Cmd>lua vim.diagnostic.open_float()<CR>", "Diagnostics in float window" },
-				D = { "<Cmd>Telescop diagnostics<CR>", "Diagnostics in Telescope" },
-				f = { "<Cmd>lua vim.lsp.buf.formatting()<CR>", "  Format" },
-				i = { "<Cmd>LspInfo<CR>", "  Info" },
-				l = { "<Cmd>lua vim.lsp.codelens.run()<CR>", "CodeLens Action" },
-				L = { "<Cmd>lua vim.diagnostic.setloclist()<CR>", "Location list" },
-				q = { "<Cmd>lua vim.diagnostic.setqflist()<CR>", "Send to Quickfix List" },
-				r = { "<Cmd>lua vim.lsp.buf.rename()<CR>", "凜  Rename" },
-				R = { "<Cmd>lua vim.lsp.buf.references()<CR>", "Ref. of word under cursor" },
-				s = { "<Cmd>lua vim.lsp.buf.document_symbol()<CR>", "  Document Symbols" },
-				S = { "<Cmd>Telescope lsp_dynamic_workspace_symbols<CR>", "  Workspace Symbols" },
-				t = { "<Cmd>lua vim.lsp.buf.type_definition()<CR>", "Type Definition" },
-			},
-
 			s = {
 				name = "   Search",
 
@@ -166,29 +123,6 @@ else
 				r = { "<Cmd>Telescope oldfiles<CR>", "  Open Recent File" },
 				R = { "<Cmd>Telescope registers<CR>", "  Registers" },
 				t = { "<Cmd>Telescope live_grep<CR>", "  Text" },
-			},
-
-			t = {
-				name = "   Terminal",
-
-				f = { "<Cmd>ToggleTerm direction=float<CR>", "  Float" },
-				g = { "<Cmd>lua lazygit_toggle()<CR>", "  LazyGit" },
-				h = { "<Cmd>ToggleTerm size=10 direction=horizontal<CR>", "  Horizontal" },
-				n = { "<Cmd>lua node_toggle()<CR>", "  Node.js" },
-				p = { "<Cmd>lua python_toggle()<CR>", "   Python" },
-				t = { "<Cmd>lua htop_toggle()<CR>", "   Htop" },
-				v = { "<Cmd>ToggleTerm size=80 direction=vertical<CR>", "   Vertical" },
-				w = { "<Cmd>lua cargo_watch_toggle()<CR>", "   Cargo Watch" },
-			},
-
-			x = {
-				name = "   Lists",
-
-				d = { "<Cmd>Trouble document_diagnostics<CR>", "Document Diagnostics" },
-				l = { "<Cmd>Trouble loclist<CR>", "Loclist" },
-				q = { "<Cmd>Trouble quickfix<CR>", "Quickfix List" },
-				r = { "<Cmd>Trouble lsp_references<CR>", "Ref. of word under cursor" },
-				w = { "<Cmd>Trouble workspace_diagnostics<CR>", "Workspace Diagnostics" },
 			},
 		},
 	})
