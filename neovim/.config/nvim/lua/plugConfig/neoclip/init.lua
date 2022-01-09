02662831373f4424f151c8539aa59c4b0b7128ee
LudoPinelli <93927273+LudoPinelli@users.noreply.github.com>
2022-01-09 15:50:21 +0100

    add: sqlite.lua | update: neoclip config

--- neovim/.config/nvim/lua/plugConfig/neoclip/init.lua
+++ neovim/.config/nvim/lua/plugConfig/neoclip/init.lua
@@ -4,12 +4,36 @@ if not status_ok then
 end
 
 neoclip.setup({
+	history = 1000,
+	enable_persistant_history = true,
+	db_path = vim.fn.stdpath("data") .. "/databases/neoclip.sqlite3",
+	filter = nil,
+	preview = true,
+	default_register = { '"' },
+	default_register_macros = "q",
+	enable_macro_history = true,
+	content_spec_column = false,
+	on_paste = {
+		set_reg = false,
+	},
+	on_replay = {
+		set_reg = false,
+	},
 	keys = {
 		telescope = {
 			i = {
-				select = "<CR>",
-				paste = "<C-p>",
-				paste_behind = "<C-P>",
+				select = "<cr>",
+				paste = "<c-p>",
+				paste_behind = "<c-a>",
+				replay = "<c-q>",
+				custom = {},
+			},
+			n = {
+				select = "<cr>",
+				paste = "p",
+				paste_behind = "P",
+				replay = "q",
+				custom = {},
 			},
 		},
 	},
--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -185,6 +185,7 @@ return packer.startup(function(use)
 	use("lewis6991/impatient.nvim") -- Improve startup time
 	use("RishabhRD/nvim-cheat.sh") -- Access to Cheat.sh
 	use("dstein64/vim-startuptime") -- Startup time info
+	use("tami5/sqlite.lua")
 
 	------------------------------------
 	--          MARKDOWN              --
