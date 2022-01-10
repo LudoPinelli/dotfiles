abeeb7c17e911f6f00d675135d0d6b3b90e0de71
LudoPinelli <93927273+LudoPinelli@users.noreply.github.com>
2022-01-10 04:37:43 +0100

    style: --

--- neovim/.config/nvim/lua/autocommands.lua
+++ neovim/.config/nvim/lua/autocommands.lua
@@ -22,7 +22,7 @@ vim.cmd([[
   augroup vimrc-remember-cursor-position
     autocmd!
     autocmd BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g`\"" | endif
-  augroup END
+  augroup end
 ]])
 
 -- Highlight on yank
