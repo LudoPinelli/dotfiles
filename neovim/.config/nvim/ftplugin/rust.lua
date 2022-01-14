43f747d9e7e552a8b7a9bf04e4626b667d9c9d5d
LudoPinelli <93927273+LudoPinelli@users.noreply.github.com>
2022-01-14 16:02:16 +0100

    disable keymaps in rust.lua until rust-tools is fixed

--- neovim/.config/nvim/ftplugin/rust.lua
+++ neovim/.config/nvim/ftplugin/rust.lua
@@ -1,12 +1,12 @@
 ------------- Keymaps ---------------
 
-local map = vim.keymap.set
-local opts = { noremap = true, silent = true }
-
-map("<Leader>rr", "<Cmd>Crun<CR>", opts)
-map("<Leader>rc", "<Cmd>Ccheck<CR>", opts)
-map("<Leader>rb", "<Cmd>Cbuild<CR>", opts)
-map("<Leader>rt", "<Cmd>Ctest<CR>", opts)
+-- local map = vim.keymap.set
+-- local opts = { noremap = true, silent = true }
+--
+-- map("<Leader>rr", "<Cmd>Crun<CR>", opts)
+-- map("<Leader>rc", "<Cmd>Ccheck<CR>", opts)
+-- map("<Leader>rb", "<Cmd>Cbuild<CR>", opts)
+-- map("<Leader>rt", "<Cmd>Ctest<CR>", opts)
 
 -------------- cmp ------------------
 
