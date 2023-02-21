8f22c03737e136dd4e9aea9d1e286b05062a31a5
LudoPinelli <93927273+LudoPinelli@users.noreply.github.com>
2023-02-21 02:02:50 +0100

    Add flit - Remove dressing

--- neovim/.config/nvim/lua/plugins/leap.lua
+++ neovim/.config/nvim/lua/plugins/leap.lua
@@ -1,6 +1,12 @@
 return {
-    "ggandor/leap.nvim",
-    config = function()
-        require('leap').add_default_mappings()
-    end
+    {
+        "ggandor/leap.nvim",
+        config = function()
+            require('leap').add_default_mappings()
+        end
+    },
+    {
+        "ggandor/flit.nvim",
+        config = true,
+    },
 }
