20f3af0e9f0739b9b87601ec3557e537879adb03
LudoPinelli <93927273+LudoPinelli@users.noreply.github.com>
2023-12-17 02:44:33 +0100

    Nvim: remove leap, add flash, add Neogit

---
+++ neovim/.config/nvim/lua/plugins/neogit.lua
@@ -0,0 +1,13 @@
+return {
+    "NeogitOrg/neogit",
+    dependencies = {
+        "nvim-lua/plenary.nvim",         -- required
+        "sindrets/diffview.nvim",        -- optional - Diff integration
+
+        "nvim-telescope/telescope.nvim", -- optional
+    },
+    -- config = true
+    opts = {
+        kind = "auto",
+    },
+}
